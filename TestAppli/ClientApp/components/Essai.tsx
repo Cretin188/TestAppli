47619d7bfdee963b787b788f428720f295ea3732
SPaviot <syl.paviot@laposte.net>
2018-05-16 11:33:43 -0400

    no message

---
+++ TestAppli/ClientApp/components/Essai.tsx
@@ -0,0 +1,28 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class Essai extends React.Component<RouteComponentProps<{}>, {}> {
+    constructor() {
+        super();
+        this.state = { name: 'essai'};
+    }
+
+    public handleOnChange(event: any): void {
+        this.setState({ name: event.target.value });
+    }
+
+    public render() {
+        return <div>
+            <h1>Essai</h1>
+
+            <p>On va tenter de faire des trucs ici</p>
+
+            <button> Test </button>
+
+            <input
+                onChange={e => this.handleOnChange(e)}
+            />
+               
+       </div>;
+    }
+}
